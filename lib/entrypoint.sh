819bd3ab7630d046a20a2c4b544c646a4b45316a
Kevin Solkowski <ksolkowski@gmail.com>
2024-02-20 17:01:53 -0600

    Update entrypoint.sh

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -8,6 +8,5 @@ gem install rubocop-minitest -v 0.13.0
 gem install rubocop-performance -v 1.11.5
 gem install rubocop-rails -v 2.11.3
 
-rubocop
-
-# ruby /action/lib/index.rb
+gem list
+ruby /action/lib/index.rb
