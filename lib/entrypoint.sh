9f3114a1311ce12ee95df961e5b8749b13e7d764
Kevin Solkowski <ksolkowski@gmail.com>
2024-02-22 14:19:25 -0600

    Update entrypoint.sh

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -8,5 +8,5 @@ gem install rubocop-minitest -v 0.13.0
 gem install rubocop-performance -v 1.11.5
 gem install rubocop-rails -v 2.11.3
 
-echo "haml-lint"
+bundle exec haml-lint
 ruby /action/lib/index.rb
